f30fad6c730535864ce82fb9e9c37553c0a11668
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2025-06-26 18:43:22 +0700

    Implement insert function

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -40,4 +40,34 @@ public:
         }
     }
 
-    
+    void insert()
+    {
+        int x;
+        cout << "Masukkan nilai :";
+        cin >> x;
+
+        // step 1 : allocated memory for the new node
+        Node *newNode = new Node();
+
+        // step 2 : assign value to the data field of new node
+        newNode->info = x;
+
+        // step 3 : make the left and right child of the new Node point to NULL
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
+
+        // step 4 : locate the node witch will be the parent of the nood to be inserted
+        Node *parent = nullptr;
+        Node *currentNode = nullptr;
+        search(x, parent, currentNode);
+
+        // step 5 : if parent is NULL (tree is empty)
+        {
+            if (parent == nullptr)
+                // 5a : allocate memory for thr new node
+                ROOT = newNode;
+
+            // 5a : exit
+            return;
+        }
+       
