97f2df378465361d5e3420360e38eeb712e9b40b
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2025-06-26 18:34:54 +0700

    Add search function to find parent and current node for a given value

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -25,4 +25,19 @@ public:
     {
         ROOT = nullptr; // initializing ROOT to null
     }
+    void search(int element, Node *&parent, Node *&currentNode)
+    {
+        // this function searches the currentNode of the specified Node as well as the current Node of its parent
+        currentNode = ROOT;
+        parent = nullptr;
+        while ((currentNode != nullptr) && (currentNode->info != element))
+        {
+            parent = currentNode;
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+            else
+                currentNode - currentNode->rightchild;
+        }
+    }
+
     
