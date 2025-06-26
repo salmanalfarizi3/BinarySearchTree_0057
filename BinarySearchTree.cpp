7d4598c7c1a956627a1b6883bd74bbd4d12b0d00
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2025-06-26 18:54:44 +0700

    Implement postorder traversal

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -124,4 +124,18 @@ public:
      preorder(ptr->rightchild);
  }
 
- 
+ void postorder(Node *ptr)
+ {
+     if (isEmpty())
+     {
+         cout << "Tree is Empty" << endl;
+         return;
+     }
+     if (ptr == nullptr)
+         return;
+     postorder(ptr->leftchild);
+     postorder(ptr->rightchild);
+     cout << ptr->info << " "; // parent
+ }
+};
+
