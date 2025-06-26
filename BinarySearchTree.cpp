28daad7a390367b8ecfbc562cd5f12d2150fefd6
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2025-06-26 18:54:03 +0700

    Implement preorder traversal

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -110,4 +110,18 @@ public:
      inorder(ptr->rightchild);
  }
 
+ void preorder(Node *ptr)
+ {
+     if (isEmpty())
+     {
+         cout << "Tree is Empty" << endl;
+         return;
+     }
+     if (ptr == nullptr)
+         return;
+     cout << ptr->info << " "; // parent
+     preorder(ptr->leftchild);
+     preorder(ptr->rightchild);
+ }
+
  
